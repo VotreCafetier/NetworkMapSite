8d7a88b8147e6dbf49ec9c5763066cfb1e2843d3
Dany Gauthier <danygauthier57@yahoo.ca>
2021-10-03 10:31:31 -0400

    Started app in django

--- NetworkMapDjango/NetworkMapDjango/settings.py
+++ NetworkMapDjango/NetworkMapDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1q4jkhumalej2_kpvgdz&9k7piwe**p)&fh+-a=c2_x@-paez7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Api.apps',
 ]
 
 MIDDLEWARE = [
